1227c65a0bbcbc9e557d81e735a137f831370516
Nidhi Chawla <s3633598@student.rmit.edu.au>
2017-09-21 21:44:12 +1000

    header and footer added

---
+++ src/footer.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+import './App.css';
+class Footer extends Component{
+    render(){
+        return (
+            <div className="footer">
+                @copyright2017
+            </div>
+        );
+
+    }
+}
+export default Footer;
